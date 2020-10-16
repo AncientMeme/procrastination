8c16f4bf94e106746e1ee26fee0945dac23cf54c
Sam <n10050965@qut.edu.au>
2020-10-16 13:27:36 +0800

    1

---
+++ js/mapscript.js
@@ -0,0 +1,48 @@
+// $('ol li').mouseover(function() {
+//     var name = this.id
+//     alert(name)
+//     $('#GeorgieM').mouseover();
+//         }).mouseout(function(e) {
+//     $('#GeorgieM').mouseout();
+//         }).click(function(e) { e.preventDefault();
+//  });
+$('#Georgie').mouseover(function(e) {
+    $('#GeorgieM').mouseover();
+        }).mouseout(function(e) {
+    $('#GeorgieM').mouseout();
+        }).click(function(e) { e.preventDefault(); });
+$('#Alex').mouseover(function(e) {
+    $('#AlexM').mouseover();
+        }).mouseout(function(e) {
+    $('#AlexM').mouseout();
+        }).click(function(e) { e.preventDefault(); });
+$('#Saki').mouseover(function(e) {
+    $('#SakiM').mouseover();
+        }).mouseout(function(e) {
+    $('#SakiM').mouseout();
+        }).click(function(e) { e.preventDefault(); });
+$('#Arkosix').mouseover(function(e) {
+    $('#ArkosixM').mouseover();
+        }).mouseout(function(e) {
+    $('#ArkosixM').mouseout();
+        }).click(function(e) { e.preventDefault(); });
+$('#Tobi').mouseover(function(e) {
+    $('#TobiM').mouseover();
+        }).mouseout(function(e) {
+    $('#TobiM').mouseout();
+        }).click(function(e) { e.preventDefault(); });
+$('#Jonjo').mouseover(function(e) {
+    $('#JonjoM').mouseover();
+        }).mouseout(function(e) {
+    $('#JonjoM').mouseout();
+        }).click(function(e) { e.preventDefault(); });
+$('#Cheezi').mouseover(function(e) {
+    $('#CheeziM').mouseover();
+        }).mouseout(function(e) {
+    $('#CheeziM').mouseout();
+        }).click(function(e) { e.preventDefault(); });
+$('#Margerit').mouseover(function(e) {
+    $('#MargeritM').mouseover();
+        }).mouseout(function(e) {
+    $('#MargeritM').mouseout();
+        }).click(function(e) { e.preventDefault(); });
